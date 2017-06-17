eeb1581d6510aab16e0054327471a94ddf1dfd89
Raj Saraogi <Raj Saraogi>
2017-06-17 16:05:26 +0530

    Added mlab and heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,8 @@ var mongoose=require('mongoose');
 
 //We will set promises to use with mongoose instead of callbaks
 mongoose.Promise=global.Promise;
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-//mongodb://<dbuser>:<dbpassword>@ds127872.mlab.com:
-//Connecting with the mongoose database
-mongoose.connect("mongodb://raj:raj@ds157641.mlab.com:27872/peepipwash");
-//mongoose.connect("mongodb://localhost:27017/PeepipApi");
+
+mongoose.connect("mongodb://raj:raj@ds157641.mlab.com:57641/todo");
 
 module.exports={
     mongoose
